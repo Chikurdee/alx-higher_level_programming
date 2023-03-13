a311f4fdbad004b07acb15c17079889ba32aff18
Ugwuanyi Christopher Chikodili <christopherchikodili@gmail.com>
2023-03-13 11:04:02 -0700

    Answers to Python - Data Structures: Lists, Tuples

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -13,12 +13,12 @@ void reverse_listint(listint_t **head)
 	listint_t *next = NULL;
 
 	while (current)
-	{
-		next = current->next;
-		current->next = prev;
-		prev = current;
-		current = next;
-	}
+		{
+			next = current->next;
+			current->next = prev;
+			prev = current;
+			current = next;
+		}
 
 	*head = prev;
 }
@@ -36,33 +36,33 @@ int is_palindrome(listint_t **head)
 	if (*head == NULL || (*head)->next == NULL)
 		return (1);
 	while (1)
-	{
-		fast = fast->next->next;
-		if (!fast)
 		{
-			dup = slow->next;
-			break;
+			fast = fast->next->next;
+			if (!fast)
+				{
+					dup = slow->next;
+					break;
+				}
+			if (!fast->next)
+				{
+					dup = slow->next->next;
+					break;
+				}
+			slow = slow->next;
 		}
-		if (!fast->next)
-		{
-			dup = slow->next->next;
-			break;
-		}
-		slow = slow->next;
-	}
 
 	reverse_listint(&dup);
 
 	while (dup && temp)
-	{
-		if (temp->n == dup->n)
 		{
-			dup = dup->next;
-			temp = temp->next;
+			if (temp->n == dup->n)
+				{
+					dup = dup->next;
+					temp = temp->next;
+				}
+			else
+				return (0);
 		}
-		else
-			return (0);
-	}
 
 	if (!dup)
 		return (1);
